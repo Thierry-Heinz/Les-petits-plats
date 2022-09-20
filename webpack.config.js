142793336c8254cc4e08dacd1659577f2799b7a1
Thierry-Heinz <studioklatz@gmail.com>
2022-09-20 12:56:25 +0200

    wip app architecture

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,7 @@
 const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 const ESLintPlugin = require("eslint-webpack-plugin");
+const CopyWebpackPlugin = require("copy-webpack-plugin");
 
 module.exports = {
   entry: {
@@ -13,10 +15,29 @@ module.exports = {
       {
         test: /\.(js)$/,
         exclude: /node_modules/,
-        use: ["babel-loader"],
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: [["@babel/preset-env", { targets: "defaults" }]],
+          },
+        },
       },
     ],
   },
   resolve: { extensions: [".js"] },
-  plugins: [new ESLintPlugin()],
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, "src") + "/index.html",
+      filename: path.resolve(__dirname, "dist") + "/index.html",
+    }),
+    new ESLintPlugin(),
+    new CopyWebpackPlugin({
+      patterns: [
+        {
+          from: path.resolve(__dirname, "src") + "/assets",
+          to: path.resolve(__dirname, "dist") + "/assets",
+        },
+      ],
+    }),
+  ],
 };
