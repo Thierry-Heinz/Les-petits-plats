54c148c0356443c5e41d2361db298273c3b51736
Thierry-Heinz <studioklatz@gmail.com>
2022-10-04 16:42:57 +0200

    updated common code for both vers. of searchAPI

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,39 +1,43 @@
 import { recipes } from "./../assets/data/data";
+import RecipeMethod from "./templates/RecipeMethod";
 import Recipe from "./models/Recipe";
 import SorterMethod from "./templates/SorterMethod";
+import SingleSorter from "./models/SingleSorter";
 import InputSearch from "./models/InputSearch";
 
 import "../sass/main.scss";
-import SingleSorter from "./models/SingleSorter";
 
 const init = () => {
-  const recipesSection = document.querySelector("#recipes");
+  const $recipesSection = document.querySelector("#recipes");
 
   const recipesObj = recipes.map((recipe, index) => {
     const recipeObj = new Recipe(recipe, index);
-    recipesSection.appendChild(recipeObj.createCard(index));
+    $recipesSection.appendChild(recipeObj.createCard(index));
     return recipeObj;
   });
 
-  const sorterMethod = new SorterMethod(recipesObj);
+  const recipeMethod = new RecipeMethod(recipesObj, $recipesSection);
+
+  const sorterMethod = new SorterMethod(recipeMethod);
+
   const ingredientsSorter = new SingleSorter(
-    sorterMethod.createIngredientsArray(),
+    sorterMethod,
     "Ingrédients",
     "ingredients"
   );
   const appliancesSorter = new SingleSorter(
-    sorterMethod.createAppliancesArray(),
+    sorterMethod,
     "Appareils",
     "appliances"
   );
   const ustensilsSorter = new SingleSorter(
-    sorterMethod.createUstensilsArray(),
+    sorterMethod,
     "Ustensiles",
     "ustensils"
   );
   const sortersArray = [ingredientsSorter, appliancesSorter, ustensilsSorter];
 
-  const mainSearch = new InputSearch(recipesObj, sortersArray, 3);
+  const mainSearch = new InputSearch(recipeMethod, sortersArray, 3);
   mainSearch.createInputSearch();
 };
 
--- src/scripts/models/SingleSorter.js
+++ src/scripts/models/SingleSorter.js
@@ -1,11 +1,37 @@
-import SorterMethod from "../templates/SorterMethod";
-
-export default class SingleSorter extends SorterMethod {
-  constructor(sorterData, title, label) {
-    super();
+export default class SingleSorter {
+  constructor(sorterMethod, title, label) {
+    this.sorterMethod = sorterMethod;
     this.title = title;
     this.label = label;
-    this.sorterData = sorterData;
-    this.$sorter = this.createSorter(title, label, sorterData);
+    this.sorterData = this.sorterMethod.createSorterData(this.label);
+    this.$sorter = this.sorterMethod.createSorter(
+      this.title,
+      this.label,
+      this.sorterData
+    );
+  }
+  updateSorterList(value, newData) {
+    this.clearSorter();
+    console.log(value);
+    const newSorterData = this.sorterMethod.createSorterData(
+      this.label,
+      newData
+    );
+    const newSorter = this.sorterMethod.createSorter(
+      this.title,
+      this.label,
+      newSorterData
+    );
+    this.sorterMethod.$sorterWrapper.appendChild(newSorter);
+    this.$sorter = newSorter;
+    value ? this.updateSorterInput(value) : "";
+  }
+  clearSorter() {
+    this.$sorter.remove();
+  }
+  updateSorterInput(value) {
+    this.$sorter
+      .querySelector(".sorter-input")
+      .setAttribute("placeholder", `${value}`);
   }
 }
